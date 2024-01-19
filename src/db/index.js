a1e76324b56ee7b1705e497d0003d5978e826032
Saurabh Rawat <saurabhrawat.official@gmail.com>
2024-01-19 23:55:36 +0530

    main: refactor code

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,16 @@
-import mongoose from 'mongoose';
-import { DB_NAME } from '../constants.js';
+import mongoose from 'mongoose'
+import { DB_NAME } from '../constants.js'
 
 const connectDB = async () => {
   try {
-    const instance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log(`\n MongoDB connected!! DB HOST:${instance.connection.host}`)
+    const instance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    )
+    console.log(`\nMongoDB connected!! DB HOST:${instance.connection.host}`)
   } catch (error) {
-    console.log('mongodb connection error', error);
+    console.log('mongodb connection error', error)
     process.exit(1)
   }
-};
+}
 
 export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,31 +1,21 @@
-// require('dotenv').config({ path: './env' })
 import dotenv from 'dotenv'
-import connectDB from './db/index.js';
+
+import connectDB from './db/index.js'
+import { app } from './app.js'
 
 dotenv.config({ path: './env' })
+
 connectDB()
-
-
-/*
-NOTE: INITIAL APPROACH TO PUT EVRYTHING AT ONE PLACE
-
-import express from 'express'
-const app = express()
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+  .then(() => {
     app.on('error', (error) => {
-        console.log('ERROR:', error)
-        throw error
+      console.log('ERROR:', error)
+      throw error
     })
 
-    app.listen(process.env.PORT, () => {
-        console.log(`App is up and running on port:${process.env.PORT}`)
-    })
-  } catch (error) {
-    console.log('ERROR:', error)
-    throw error
-  }
-})();
-*/
+    app.listen(process.env.PORT || 8000, () =>
+      console.log(
+        `Server is up and running on port:${process.env.PORT || 8000}`
+      )
+    )
+  })
+  .catch((error) => console.log('MONGODB connection failed!!', error))
